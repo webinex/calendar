ce1ae614dbb5fd18aa96b285172ec577ea411241
Siarhei Skalaban <siarhei.skalaban@gmail.com>
2023-06-01 21:07:27 +0300

    IMPROVE: Additional recurrent event methods

---
+++ src/Webinex.Calendar.Tests/RecurrentEventTests/RecurrentEventTests_LastPeriod_Interval.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Webinex.Calendar.Common;
+using Webinex.Calendar.Events;
+
+namespace Webinex.Calendar.Tests.RecurrentEventTests;
+
+// ReSharper disable once InconsistentNaming
+public class RecurrentEventTests_LastPeriod_Interval
+{
+    private RecurrentEvent<object> _subject = null!;
+
+    [Test]
+    public void WhenBeforeEffectiveStart_ShouldReturnNull()
+    {
+        _subject.LastPeriod(_subject.Effective.Start.AddMinutes(-1)).Should().BeNull();
+    }
+
+    [Test]
+    public void WhenAfterEffectiveEnd_ShouldReturnLastEffective()
+    {
+        var result = _subject.LastPeriod(_subject.Effective.End!.Value.AddYears(1));
+        result.Should().BeEquivalentTo(
+            new Period(
+                JAN1_2023_UTC.Day(29).TotalMinuteOfDay(600),
+                JAN1_2023_UTC.Day(29).TotalMinuteOfDay(600 + 60)));
+    }
+
+    [Test]
+    public void WhenMatchBeforeEffectiveEnd_ShouldReturnMatch()
+    {
+        var result = _subject.LastPeriod(_subject.Effective.Start.Day(17));
+        result.Should().BeEquivalentTo(
+            new Period(
+                JAN1_2023_UTC.Day(15).TotalMinuteOfDay(600),
+                JAN1_2023_UTC.Day(15).TotalMinuteOfDay(600 + 60)));
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        _subject = RecurrentEvent<object>.NewInterval(
+            JAN1_2023_UTC.AddMinutes(600),
+            JAN1_2023_UTC.AddMonths(1),
+            7 * 24 * 60, // every week
+            60,
+            new object());
+    }
+}
---
+++ src/Webinex.Calendar.Tests/RecurrentEventTests/RecurrentEventTests_MatchPeriod_Interval.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Webinex.Calendar.Common;
+using Webinex.Calendar.Events;
+
+namespace Webinex.Calendar.Tests.RecurrentEventTests;
+
+// ReSharper disable once InconsistentNaming
+public class RecurrentEventTests_MatchPeriod_Interval
+{
+    private RecurrentEvent<object> _subject = null!;
+
+    [Test]
+    public void WhenBeforeEffectiveStart_ShouldReturnNull()
+    {
+        _subject.MatchPeriod(_subject.Effective.Start.AddMinutes(-1)).Should().BeNull();
+    }
+
+    [Test]
+    public void WhenMatchFirst_ShouldReturnStartPeriod()
+    {
+        var result = _subject.MatchPeriod(_subject.Effective.Start);
+        result.Should().BeEquivalentTo(
+            new Period(
+                JAN1_2023_UTC,
+                JAN1_2023_UTC.TotalMinuteOfDay(60)));
+    }
+
+    [Test]
+    public void WhenAfterEffectiveEnd_ShouldReturnNull()
+    {
+        _subject.MatchPeriod(_subject.Effective.End!.Value.AddMinutes(-1)).Should().BeNull();
+    }
+
+    [Test]
+    public void WhenMatchEffectiveEnd_ShouldReturnNull()
+    {
+        _subject.MatchPeriod(_subject.Effective.End!.Value).Should().BeNull();
+    }
+
+    [Test]
+    public void WhenMatchLastPeriodEnd_ShouldReturnNull()
+    {
+        _subject.MatchPeriod(_subject.LastPeriod(_subject.Effective.End!.Value)!.End).Should().BeNull();
+    }
+
+    [Test]
+    public void WhenMatchLastPeriodStart_ShouldReturnMatch()
+    {
+        var lastPeriod = _subject.LastPeriod(_subject.Effective.End!.Value)!;
+        var result = _subject.MatchPeriod(lastPeriod.Start);
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(lastPeriod);
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        _subject = RecurrentEvent<object>.NewInterval(
+            JAN1_2023_UTC,
+            JAN1_2023_UTC.AddMonths(1),
+            7 * 24 * 60, // every week
+            60,
+            new object());
+    }
+}
