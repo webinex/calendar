838acbcc54032e90f8d3480e648f1eddf74ddcae
KyMback <Seregick@hotmail.com>
2024-10-29 15:41:40 +0300

    FIX: fixed NullReferenceException when moved visit is outside of effective period

--- src/Webinex.Calendar/Filters/DbQuery.cs
+++ src/Webinex.Calendar/Filters/DbQuery.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Webinex.Calendar.Common;
 using Webinex.Calendar.DataAccess;
+using Webinex.Calendar.Events;
 
 namespace Webinex.Calendar.Filters;
 
@@ -61,11 +62,59 @@ internal class DbQuery<TData> where TData : class, ICloneable
             Precise: _filteringOptionsFlags.HasFlag(DbFilterOptimization.Precise));
 
         var dbResult = await queryable.Where(provider.Create()).ToArrayAsync();
+        await PopulateStatesWithRecurrentEvent(queryable, dbResult);
 
         // after not precise db filtering we should do precise filtering on the client
         provider.Precise = true;
         provider.Data = true;
 
         return dbResult.Where(provider.Create().Compile()).ToArray();
+    }
+
+    /// <summary>
+    /// In rare cases EventRows of type RecurrentEventState might not have assigned RecurrentEvent, but have RecurrentEventId.
+    /// In these cases we manually find RecurrentEvents and assign them
+    /// </summary>
+    private async Task PopulateStatesWithRecurrentEvent(
+        IQueryable<EventRow<TData>> queryable,
+        EventRow<TData>[] rows)
+    {
+        var statesWithoutRecurrentEvent = rows
+            .Where(e => e.Type == EventType.RecurrentEventState)
+            .Where(e => e.RecurrentEvent == null)
+            .Where(e => e.RecurrentEventId.HasValue)
+            .ToArray();
+
+        var requiredRecurrentEvents = statesWithoutRecurrentEvent
+            .Select(e => e.RecurrentEventId)
+            .OfType<Guid>()
+            .Distinct()
+            .ToArray();
+
+        if (requiredRecurrentEvents.Length == 0)
+            return;
+
+        var localRecurrentEvents = rows
+            .Where(e => e.Type == EventType.RecurrentEvent)
+            .Where(e => requiredRecurrentEvents.Contains(e.Id))
+            .ToDictionary(e => e.Id);
+
+        var surplusRecurrentEvents = requiredRecurrentEvents.Except(localRecurrentEvents.Select(e => e.Key)).ToArray();
+        var dbRecurrentEvents = surplusRecurrentEvents.Length == 0
+            ? new Dictionary<Guid, EventRow<TData>>(0)
+            : await queryable.Where(e => surplusRecurrentEvents.Contains(e.Id)).ToDictionaryAsync(e => e.Id);
+
+        foreach (var stateRow in statesWithoutRecurrentEvent)
+        {
+            var recurrentEventId = stateRow.RecurrentEventId!.Value;
+            if (!localRecurrentEvents.TryGetValue(recurrentEventId, out var recurrentEvent) &&
+                !dbRecurrentEvents.TryGetValue(recurrentEventId, out recurrentEvent))
+            {
+                throw new InvalidOperationException(
+                    $"Unable to find RecurrentEvent '{recurrentEventId}' for RecurrentEventState '{stateRow.Id}'");
+            }
+
+            stateRow.RecurrentEvent = recurrentEvent;
+        }
     }
 }
