96c949c93037f3ed449804f7eca4aca8de3cd82a
Siarhei Skalaban <s.skalaban@itransition.com>
2026-05-04 11:31:56 +0300

    2.0.0-build3

--- src/Webinex.Calendar/Services/OccurenceReadService.cs
+++ src/Webinex.Calendar/Services/OccurenceReadService.cs
@@ -11,6 +11,11 @@ internal interface IOccurrenceReadService<TData>
         FilterRule? dataFilterRule = null,
         bool tryCache = false);
 
+    Task<IReadOnlyCollection<Occurrence<TData>>> MaterializedOccurrencesAsync(
+        FilterRule? filterRule = null,
+        IEnumerable<SortRule>? sortRules = null,
+        PagingRule? pagingRule = null);
+
     Task<IReadOnlyCollection<Occurrence<TData>>> OccurrencesAsync(IEnumerable<string> ids, bool tryCache = false);
 }
 
@@ -39,6 +44,37 @@ internal class OccurrenceReadService<TData> : IOccurrenceReadService<TData>
         return dataFilterRule == null ? result : FilterByDataFilterRule(result, dataFilterRule);
     }
 
+    public async Task<IReadOnlyCollection<Occurrence<TData>>> MaterializedOccurrencesAsync(
+        FilterRule? filterRule = null,
+        IEnumerable<SortRule>? sortRules = null,
+        PagingRule? pagingRule = null)
+    {
+        var rows = await _eventRepository.GetAllAsync(
+            EventEntityType.OneTimeEvent | EventEntityType.OccurrenceAdjustment,
+            filterRule,
+            sortRules, pagingRule);
+
+        return Map().ToArray();
+
+        IEnumerable<Occurrence<TData>> Map()
+        {
+            foreach (var entity in rows)
+                switch (entity)
+                {
+                    case Event<TData> @event:
+                        yield return OccurrenceCalculator<TData>.CalculateOneTime(@event);
+                        break;
+                    case OccurrenceAdjustment<TData> adjustment:
+                        if (OccurrenceCalculator<TData>.TryCalculateOccurrenceAdjustment(adjustment, out var occurrence))
+                            yield return occurrence;
+                        break;
+                    default:
+                        throw new InvalidOperationException(
+                            $"Unexpected entity type {entity.GetType().FullName} for materialized occurrences");
+                }
+        }
+    }
+
     public async Task<IReadOnlyCollection<Occurrence<TData>>> OccurrencesAsync(
         IEnumerable<string> ids,
         bool tryCache = false)
@@ -52,11 +88,11 @@ internal class OccurrenceReadService<TData> : IOccurrenceReadService<TData>
     private Occurrence<TData> MapOccurrence(OccurrenceId id, IReadOnlyCollection<IEventEntityBase> entities)
     {
         var adjustment = entities.OfType<OccurrenceAdjustment<TData>>().FirstOrDefault(x => x.Id == id.ToString());
-        
+
         if (adjustment != null &&
             OccurrenceCalculator<TData>.TryCalculateOccurrenceAdjustment(adjustment, out var result1))
             return result1;
-        
+
         var @event = entities.OfType<Event<TData>>().FirstOrDefault(x => x.Id == id.EventId);
         @event = @event ?? throw new InvalidOperationException($"Unable to find event for occurrence id {id}");
 
