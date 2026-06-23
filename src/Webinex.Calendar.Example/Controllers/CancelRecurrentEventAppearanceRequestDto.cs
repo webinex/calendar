ce682306fdc57b74af63a88923fc0c3d4d565e51
Siarhei Skalaban <s.skalaban@itransition.com>
2026-06-23 07:17:40 +0300

    WIP: Delete example project

--- src/Webinex.Calendar.Example/Controllers/CancelRecurrentEventAppearanceRequestDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Webinex.Calendar.Example.Controllers;
-
-public class CancelRecurrentEventAppearanceRequestDto
-{
-    public Guid RecurrentEventId { get; init; }
-    public DateTimeOffset EventStart { get; init; }
-}
--- src/Webinex.Calendar.Example/Controllers/EditEventTimeRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Webinex.Calendar.Example.Controllers;
-
-public class EditEventTimeRequestDto
-{
-    public Guid RecurrentEventId { get; init; }
-    public DateTimeOffset EventStart { get; init; }
-    public DateTimeOffset MoveToStart { get; init; }
-    public DateTimeOffset MoveToEnd { get; init; }
-}
